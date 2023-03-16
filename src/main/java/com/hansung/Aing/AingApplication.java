8f536e55b70509cfdeabdaeffb81c2f0c4efb03d
LeeMyeongSeong <goodthinglee@gmail.com>
2023-03-17 02:32:01 +0900

    Initial commit

---
+++ src/main/java/com/hansung/Aing/AingApplication.java
@@ -0,0 +1,14 @@
+package com.hansung.Aing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AingApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hansung/Aing/AingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansung.Aing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
